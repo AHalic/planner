b3b1c39d20394256100008ee4e421fd08616cec1
AHalic <sophie.gama@aluno.ufes.br>
2024-07-14 00:13:16 -0400

    :sparkles: Adds activities routes

---
+++ backend/src/routes/create-activity.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
+
+export async function createActivity(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .post('/trip/:tripId/activity', {
+            schema: {
+                params: z.object({
+                    tripId: z.string().uuid(),
+                }),
+                body: z.object({
+                    name: z.string().min(3),
+                    date: z.coerce.date(),
+                })
+            }
+        }, async (request) => {
+            const { name, date } = request.body
+            const { tripId } = request.params
+            
+            const trip = await prisma.trip.findUnique({
+                where: { id: tripId }
+            })
+
+            if (!trip) {
+                throw new Error('Trip not found')
+            }
+
+            if (dayjs(date).isBefore(trip.startDate) || dayjs(date).isAfter(trip.endDate)) {
+                throw new Error('The activity must occur within the trip dates')
+            }
+
+            const activity = await prisma.activity.create({
+                data: {
+                    name,
+                    date,
+                    trip: {
+                        connect: { id: tripId }
+                    }
+                }
+            })
+
+            return { id: activity.id }
+        })
+}
---
+++ backend/src/routes/get-activities.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
+
+
+export async function getActivities(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .get('/trip/:tripId/activity', {
+            schema: {
+                params: z.object({
+                    tripId: z.string().uuid(),
+                }),
+            }
+        }, async (request) => {
+            const { tripId } = request.params
+            
+            const trip = await prisma.trip.findUnique({
+                where: { id: tripId },
+                include: {
+                    activities: {
+                        orderBy: {
+                            date: 'asc'
+                        }
+                    }
+                }
+            })
+
+            if (!trip) {
+                throw new Error('Trip not found')
+            }
+
+            const amountOfDaysInTrip = dayjs(trip.endDate).diff(dayjs(trip.startDate), 'day') + 1
+            const activities = Array.from({ length: amountOfDaysInTrip }, 
+                // map function
+                (_, index) => {
+                    const currentDate = dayjs(trip.startDate).add(index, 'day')
+
+                    const activitiesForDay = trip.activities.filter(activity => {
+                        return dayjs(activity.date).isSame(currentDate, 'day')
+                    })
+
+                    return {
+                        date: currentDate.toDate(),
+                        activities: activitiesForDay
+                    }
+                })
+
+            return { activities }
+        })
+}
